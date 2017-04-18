af0522af377e4f5cb2274ba2e072d02418d9817f
Jacob Copley <yelpocbocaj@gmail.com>
2017-04-18 09:25:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const util = require('util')
+const fs = require('fs')
+
+const len = fs.readFileSync('list_length.scm', 'utf8')
+
+function walk(arr){
+	for(let i=0; i<arr.length; i++){
+		if(Array.isArray(arr[i])){
+			walk(arr[i])
+		} else {
+			arr[i] = arr[i].split(' ')
+		}
+	}
+	return arr
+}
+
+function tokenizer(str){
+	str = str.replace(/\(/g, '",["')
+    str = str.replace(/\)/g, '"],"')
+    str = str.replace(/\n/g, ' ')
+    str = str.replace(/\s{2,}/g, ' ')
+    str = str.replace(/,""|"",|," "/g, '')
+    str = str.replace(/ "|" /g, '"')
+    str = str.replace(/^",|,"$/g, '')
+
+	return walk(JSON.parse(str))
+}
+
+console.log(util.inspect(tokenizer(len), {depth: null}))
+
+
+
+/*
+const fact = `(define fact (lambda (x) (if (= x 1) 1 (* x (fact (- x 1))))))`
+const member = `(define member? (lambda (a lat) (cond ((null? lat) #f) (else (or (eq? (car lat) a) (member? a (cdr lat)))))))`
+*/
---
+++ list_length.scm
@@ -0,0 +1,15 @@
+(define listLength
+    (lambda (passedlist)
+        (let ((i 1))
+            (define listLengthLoop
+                (lambda (l)
+                    (cond
+                        ((null? (cdr l)) i)
+                        (else (set! i (+ i 1)) (listLengthLoop (cdr l)))
+                    )
+                )
+            )
+            (listLengthLoop passedlist)
+        )
+    )
+)
