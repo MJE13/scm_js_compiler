0d0fbaace42d350ff1d39d3f7d090e04d15c75a5
MJE13 <morgan.eichwald@gmail.com>
2017-04-18 11:05:20 -0600

    SC-1 tree able to process multiple scm funcs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const util = require('util')
 const fs = require('fs')
 
 const len = fs.readFileSync('list_length.scm', 'utf8')
+const schemerFuncs = fs.readFileSync('little_schemer_functions.scm', 'utf8')
 
 function walk(arr){
 	for(let i=0; i<arr.length; i++){
@@ -27,15 +28,11 @@ function tokenizer(str){
     str = str.replace(/,""|"",|," "/g, '')
     str = str.replace(/ "|" /g, '"')
     str = str.replace(/^",|,"$/g, '')
-
+    str = str.replace(/^/, '[')
+    str = str.replace(/$/, ']')
 	return walk(JSON.parse(str))
 }
 
-console.log(util.inspect(tokenizer(len), {depth: null}))
+console.log(util.inspect(tokenizer(schemerFuncs), {depth: null}))
 
 
-
-/*
-const fact = `(define fact (lambda (x) (if (= x 1) 1 (* x (fact (- x 1))))))`
-const member = `(define member? (lambda (a lat) (cond ((null? lat) #f) (else (or (eq? (car lat) a) (member? a (cdr lat)))))))`
-*/
